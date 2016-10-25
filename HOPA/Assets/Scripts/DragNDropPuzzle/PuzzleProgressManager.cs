45add162694523ab1462c373dcef4eaba4361faa
Jan Bełczącki <jan.belczacki@o2.pl>
2016-10-25 21:02:54 +0200

    [+] Drag N Drop Puzzle MiniGame with two elements (for now, but there can be even more! :O)

---
+++ HOPA/Assets/Scripts/DragNDropPuzzle/PuzzleProgressManager.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PuzzleProgressManager : MonoBehaviour {
+
+    public static PuzzleProgressManager Instance;
+
+    public PuzzleElement[] Elements;
+    public int ElementsOnSlots;
+    public bool HasWon;
+
+	// Use this for initialization
+	void Start () {
+        Instance = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(!HasWon)
+        {
+            CheckProgress();
+        }
+
+        if(Input.GetKey(KeyCode.R))
+        {
+            ResetGame();
+        }
+	}
+
+    void CheckProgress()
+    {
+        if(ElementsOnSlots >= Elements.Length)
+        {
+            WinGame();
+        }
+    }
+
+    void WinGame()
+    {
+        HasWon = true;
+        Debug.Log("Hooray! You won!");
+    }
+
+    void ResetGame()
+    {
+        ElementsOnSlots = 0;
+        HasWon = false;
+
+        foreach(PuzzleElement puzzle in Elements)
+        {
+            puzzle.ResetElement();
+        }
+    }
+}
