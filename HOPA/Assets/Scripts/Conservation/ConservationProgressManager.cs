3a979ff8deb9a8e3f8d67a951e001498e46e7b8d
Jan Bełczącki <jan.belczacki@o2.pl>
2016-10-26 22:03:18 +0200

    [+] Added Conservation minigame but it's not fully finished - there is some bug with detecting pixels under cursor [+] Copied one of the images to make it Writeable

---
+++ HOPA/Assets/Scripts/Conservation/ConservationProgressManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConservationProgressManager : MinigameProgressManager<Dust> {
+
+    public static ConservationProgressManager Instance;
+
+    public Dust DustyImage;
+
+    private float _winCondition;
+
+    // Use this for initialization
+    void Start () {
+        Instance = this;
+	}
+	
+	// Update is called once per frame
+	public override void Update () {
+        base.Update();
+	}
+
+    public override void CheckProgress()
+    {
+        _winCondition = (float)ElementsCompleted / (float)DustyImage.GetPixelsCount();
+        if (_winCondition >= 0.75f)
+        {
+            DustyImage.IsClear = true;
+            WinGame();
+        }
+    }
+
+    public override void ResetGame()
+    {
+        base.ResetGame();
+
+        DustyImage.Reset();
+
+    }
+}
