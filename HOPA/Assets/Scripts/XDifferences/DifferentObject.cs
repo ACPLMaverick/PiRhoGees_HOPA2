4afd32dc4f37fd9b5f61a7c3717521b440ff9866
Jan Bełczącki <jan.belczacki@o2.pl>
2016-10-26 13:17:31 +0200

    [+] Added marking animation when we select different object

--- HOPA/Assets/Scripts/XDifferences/DifferentObject.cs
+++ HOPA/Assets/Scripts/XDifferences/DifferentObject.cs
@@ -1,13 +1,19 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class DifferentObject : MonoBehaviour {
 
     public bool IsFound;
 
+    private Coroutine _redMarkAnimation;
+    private Image _redMark;
+
 	// Use this for initialization
 	void Start () {
         InputManager.Instance.OnInputClickDown.AddListener(DetectClick);
+
+        _redMark = GetComponentInChildren<Image>();
 	}
 	
 	// Update is called once per frame
@@ -17,17 +23,37 @@ public class DifferentObject : MonoBehaviour {
 
     public void DetectClick(Vector2 screenPos, Collider2D hitCollider2D)
     {
-        IsFound = true;
-        DifferencesProgressManager.Instance.ElementsCompleted += 1;
-        this.GetComponent<SpriteRenderer>().enabled = false;
-        this.GetComponent<BoxCollider2D>().enabled = false;
+        if(!IsFound)
+        {
+            IsFound = true;
+            DifferencesProgressManager.Instance.ElementsCompleted += 1;
+
+            _redMarkAnimation = StartCoroutine(RedMarkAnimation());
+
+            this.GetComponent<BoxCollider2D>().enabled = false;
+        }
     }
 
     public void Reset()
     {
         IsFound = false;
 
-        this.GetComponent<SpriteRenderer>().enabled = true;
         this.GetComponent<BoxCollider2D>().enabled = true;
+        _redMark.fillAmount = 0;
+    }
+
+    public IEnumerator RedMarkAnimation()
+    {
+        float animationLength = 0.2f;
+        float currentTime = 0.0f;
+
+        while(currentTime <= animationLength)
+        {
+
+            _redMark.fillAmount = Mathf.Lerp(0.0f, 1.0f, currentTime / animationLength);
+
+            currentTime += Time.fixedDeltaTime;
+            yield return null;
+        }
     }
 }
